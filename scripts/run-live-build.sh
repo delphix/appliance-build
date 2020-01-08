774951a8d7b5f9ad351d8ad2cf00236661a5b0f5
George Wilson <george.wilson@delphix.com>
2020-01-07 19:02:10 -0500

    DLPX-67937 need OCI image

--- scripts/run-live-build.sh
+++ scripts/run-live-build.sh
@@ -242,6 +242,7 @@ esx) vm_artifact_ext=ova ;;
 gcp) vm_artifact_ext=gcp.tar.gz ;;
 hyperv) vm_artifact_ext=vhdx ;;
 kvm) vm_artifact_ext=qcow2 ;;
+oci) vm_artifact_ext=qcow2 ;;
 *)
 	echo "Invalid platform"
 	exit 1
