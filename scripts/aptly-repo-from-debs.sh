ccd940c6b8be2ad0c3d81e4f97f1a4d3d232c7e4
Prakash Surya <prakash.surya@delphix.com>
2020-08-24 08:47:32 -0700

    [Backport to 6.0.6.0] Enable generation of "sparse" upgrade image
    This change adds a new script that can be used to generate a "sparse"
    upgrade image. For example, given two input upgrade images, we can
    generate a sparse image like so:
    
        $ ./scripts/aptly-repo-from-image-diff.sh ../master/internal-dev.upgrade.tar ../6.0-release/internal-dev.upgrade.tar
        $ ./scripts/upgrade-image-from-aptly-repo.sh sparse-internal-dev
        $ ls -l build/artifacts/sparse-internal-dev.upgrade.tar
        -rw-r--r-- 1 delphix staff 5459210240 Jul 31 19:47 build/artifacts/sparse-internal-dev.upgrade.tar
    
    Additionally, we can inspect this new artifact and verify the set of
    packages it contains:
    
        $ tar -xf sparse-internal-dev.upgrade.tar
        $ tar -xf payload.tar.gz
        $ find pool -type f -name '*.deb' | wc -l
        199
    
    And contrast this with the number of packages contained in both the
    "master" and "6.0-release" images:
    
     - master:
    
         $ tar -xf sparse-internal-dev.upgrade.tar
         $ tar -xf payload.tar.gz
         $ find pool -type f -name '*.deb' | wc -l
         1042
    
     - 6.0/release:
    
         $ tar -xf sparse-internal-dev.upgrade.tar
         $ tar -xf payload.tar.gz
         $ find pool -type f -name '*.deb' | wc -l
         1080

---
+++ scripts/aptly-repo-from-debs.sh
@@ -0,0 +1,82 @@
+#!/bin/bash -ex
+#
+# Copyright 2018-2020 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# This script is responsible for generating a new Aptly repository by
+# taking and combining all of the deb.tar.gz tarballs produced by live
+# build for this variant (of which there will be one per supported
+# platform). The generated repository will be stored at ~/.aptly/public.
+# This new repository can then be used to generate a new upgrade image,
+# by running the "upgrade-image-from-aptly-repo.sh" script.
+#
+
+. "${BASH_SOURCE%/*}/common.sh"
+
+if [[ -z "$TOP" ]]; then
+	echo "Must be run inside the git repsitory."
+	exit 1
+fi
+
+if [[ $# -ne 1 ]]; then
+	echo "Must specify a single variant." 1>&2
+	exit 1
+fi
+
+cd "$TOP/upgrade"
+APPLIANCE_VARIANT=$1
+
+rm -rf ~/.aptly
+rm -rf debs
+mkdir debs
+
+#
+# For upgrade images that we ship to customers, we will need to include
+# the packages for every platform that we support. Building for every
+# platform can be time-comsuming though, so for developer convenience,
+# here we just take the artifacts from the live-build stage for whatever
+# appliance versions were built (making sure that we built for at least
+# one platform), and build an upgrade image from that.
+#
+LIVE_BUILD_OUTPUT_DIR="$TOP/live-build/build/artifacts"
+if ! compgen -G "$LIVE_BUILD_OUTPUT_DIR/$APPLIANCE_VARIANT*.debs.tar.gz"; then
+	echo "No live-build artifacts found for this variant" >&2
+	exit 1
+fi
+for deb_tarball in "$LIVE_BUILD_OUTPUT_DIR/$APPLIANCE_VARIANT"*.debs.tar.gz; do
+	tar xf "$deb_tarball" -C debs
+done
+
+#
+# Download the delphix upgrade verification debian package.
+# Note, we always pull from the "master" build of the verification
+# package, no matter what the UPSTREAM_BRANCH of the appliance is that
+# we're building; this is intentional.
+#
+
+AWS_S3_URI_UPGRADE_VERIFICATION=$(resolve_s3_uri \
+	"$AWS_S3_URI_UPGRADE_VERIFICATION" \
+	"$AWS_S3_PREFIX_UPGRADE_VERIFICATION" \
+	"devops-gate/master/upgrade-verify/master/post-push/release/latest")
+
+download_delphix_s3_debs "$TOP/upgrade/debs" "$AWS_S3_URI_UPGRADE_VERIFICATION"
+
+#
+# Generate an Aptly/APT repository
+#
+aptly repo create -distribution=bionic -component=delphix upgrade-repository
+aptly repo add upgrade-repository debs
+aptly publish repo -skip-contents -skip-signing upgrade-repository
