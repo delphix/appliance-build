56ba6fdcca1558a3c25fe783420d33e0414b4ce5
Prakash Surya <prakash.surya@delphix.com>
2020-12-03 13:45:43 -0800

    Github Action "::set-env" is no longer supported (#501)
    The Github Action "::set-env" command is no longer supported and results
    in failures when it is used. This change updates our scripts to remove
    the usage of that command, and use the "GITHUB_ENV" interface instead.
    
    Closes #495

--- .github/scripts/install-ansible-lint.sh
+++ .github/scripts/install-ansible-lint.sh
@@ -6,10 +6,10 @@ git checkout v3.4.21
 git branch -D master
 
 #
-# GitHub Actions exposes some "debugging commands" that can be used to
+# GitHub Actions exposes the GITHUB_ENV file that can be used to
 # manipulate the environment of the job that's running. In this case, we
-# use the "set-env" command to modify the environment of the job, to
-# edit the PATH and PYTHONPATH global variables.
+# use it to modify the environment of the job, to edit the PATH and
+# PYTHONPATH global variables.
 #
-echo "::set-env name=PATH::${PATH}:/opt/ansible-lint/bin"
-echo "::set-env name=PYTHONPATH::${PYTHONPATH}:/opt/ansible-lint/lib"
+echo "PATH=${PATH}:/opt/ansible-lint/bin" >> ${GITHUB_ENV}
+echo "PYTHONPATH=${PYTHONPATH}:/opt/ansible-lint/lib" >> ${GITHUB_ENV}
