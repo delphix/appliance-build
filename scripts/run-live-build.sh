e2a99dbac1c418fb2ac41bba1888190d6c97d63e
Palash Gandhi <palash.gandhi@delphix.com>
2023-05-01 16:55:09 -0700

    DLPX-85904 Remove hardcoded git branch from appliance-build
    PR URL: https://www.github.com/delphix/appliance-build/pull/726

--- scripts/run-live-build.sh
+++ scripts/run-live-build.sh
@@ -15,6 +15,8 @@
 # limitations under the License.
 #
 
+. "${BASH_SOURCE%/*}/common.sh"
+
 TOP=$(git rev-parse --show-toplevel 2>/dev/null)
 
 if [[ -z "$TOP" ]]; then
@@ -142,23 +144,7 @@ done
 # Set UPSTREAM_BRANCH. This will determine which version of the linux package
 # mirror is used.
 #
-if [[ -z "$UPSTREAM_PRODUCT_BRANCH" ]]; then
-	echo "UPSTREAM_PRODUCT_BRANCH is not set."
-	if ! source "$TOP/branch.config" 2>/dev/null; then
-		echo "No branch.config file found in repo root."
-		exit 1
-	fi
-
-	if [[ -z "$UPSTREAM_BRANCH" ]]; then
-		echo "UPSTREAM_BRANCH parameter was not sourced from branch.config." \
-			"Ensure branch.config is properly formatted with e.g." \
-			"UPSTREAM_BRANCH=\"<upstream-product-branch>\""
-		exit 1
-	fi
-	echo "Defaulting to branch $UPSTREAM_BRANCH set in branch.config."
-else
-	UPSTREAM_BRANCH="$UPSTREAM_PRODUCT_BRANCH"
-fi
+UPSTREAM_BRANCH=$(get_upstream_or_fail_if_unset) || exit 1
 echo "Running with UPSTREAM_BRANCH set to ${UPSTREAM_BRANCH}"
 
 pkg_mirror_secondary=''
