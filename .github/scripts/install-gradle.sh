b9d8187887c78a2457235e8dc4dcac0c9253ce2c
Sebastien Roy <sebastien.roy@perforce.com>
2025-04-28 13:58:46 -0400

    run a newer version of gradle

--- .github/scripts/install-gradle.sh
+++ .github/scripts/install-gradle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-wget -nv https://services.gradle.org/distributions/gradle-5.1-bin.zip
-sha256sum -c <<< '7506638a380092a0406364c79d6c87d03d23017fc25a5770379d1ce23c3fcd4d  gradle-5.1-bin.zip'
-unzip -d /opt gradle-5.1-bin.zip
-rm gradle-5.1-bin.zip
+wget -nv https://services.gradle.org/distributions/gradle-8.14-bin.zip
+sha256sum -c <<< '61ad310d3c7d3e5da131b76bbf22b5a4c0786e9d892dae8c1658d4b484de3caa  gradle-8.14-bin.zip'
+unzip -d /opt gradle-8.14-bin.zip
+rm gradle-8.14-bin.zip
