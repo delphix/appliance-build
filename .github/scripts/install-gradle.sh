ff9bbb78336a381769fa04859347154c2a6dc5e2
Sebastien Roy <sebastien.roy@perforce.com>
2025-04-29 13:02:46 -0400

    Upgrade gradle to 5.6.4

--- .github/scripts/install-gradle.sh
+++ .github/scripts/install-gradle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-wget -nv https://services.gradle.org/distributions/gradle-8.14-bin.zip
-sha256sum -c <<< '61ad310d3c7d3e5da131b76bbf22b5a4c0786e9d892dae8c1658d4b484de3caa  gradle-8.14-bin.zip'
-unzip -d /opt gradle-8.14-bin.zip
-rm gradle-8.14-bin.zip
+wget -nv https://services.gradle.org/distributions/gradle-5.6.4-bin.zip
+sha256sum -c <<< '1f3067073041bc44554d0efe5d402a33bc3d3c93cc39ab684f308586d732a80d  gradle-5.6.4-bin.zip'
+unzip -d /opt gradle-5.6.4-bin.zip
+rm gradle-5.6.4-bin.zip
